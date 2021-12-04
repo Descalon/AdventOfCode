dcbf05841c975e2351a2ec860b43d9aad8a696e3
Nico Glas <na.glas@belastingdienst.nl>
2021-12-04 16:42:50 +0100

    completed day 4

--- Data.fsx
+++ Data.fsx
@@ -4,3 +4,6 @@ let private readTransformFile fn = readFile >> List.map fn
 let sonarData       () = readTransformFile int "./Datafiles/day1.txt"
 let movementData    () = readFile "./Datafiles/day2.txt"
 let powerData       () = readFile "./Datafiles/day3.txt"
+let bingoDrawData   () = readTransformFile int "./Datafiles/day4Draws.txt"
+let bingoCardData   () = readFile "./Datafiles/day4Cards.txt"
+//let bingoData       () = bingoDrawData(),bingoCardData()
