422662a5f7eb37be4dea093d8089f59f0cca43be
Nico Glas <info@nicoglas.com>
2024-12-02 00:00:16 +0100

    Now with actual sorting!

--- Day1.fsx
+++ Day1.fsx
@@ -14,15 +14,17 @@ let s1 = List.sort testList1
 let s2 = List.sort testList2
 let absDiff (a:int,b:int) = Math.Abs (a-b)
 
+let sort (l,r) = List.sort l, List.sort r
+
 let zipmap (l,r) = List.zip l r |> List.map (absDiff)
-let solution = zipmap >> List.sum
+let solution = sort >> zipmap >> List.sum
 
 let s = zipmap (s1,s2)
 
 let expectedList = [2;1;0;1;2;5]
 Expect.equal s expectedList "Zip error somewhere"
 
-let actual = solution (s1,s2)
+let actual = solution (testList1, testList2)
 Expect.equal actual 11 "Summation error"
 
-// InputData.day1 () |> zipmap |> List.sum |> print
+InputData.day1 () |> solution  |> print
