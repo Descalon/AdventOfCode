73a0401fada072e9ddf8a7c4fddec28f4ff22c90
Nico Glas <info@nicoglas.com>
2024-12-02 00:14:46 +0100

    day1: slight refactor
    now have a solution to throw against the
    actual puzzle input

--- Day1.fsx
+++ Day1.fsx
@@ -10,15 +10,14 @@ let mutable print = (fun (result:int) -> printResult 1 1 result)
 let left = [3;4;2;1;3;3]
 let right = [4;3;5;3;9;3]
 
-let count l x = l |> List.filter ((=)x) |> List.length
+let solution (l,r) = 
+  let count xs x = xs |> List.filter ((=)x) |> List.length
 
-let countRight = count right
+  let folder s i = 
+    s + ((count r i) * i)
 
-let folder s i = 
-  s + ((countRight i) * i)
+  l |> List.fold folder 0
 
-let actual = 
-  left 
-  |> List.fold folder 0
+let actual = solution (left,right)
 
 Expect.equal actual 31 "Something went wrong with folding"
