7cfed5d6c886ba414d35df334fc98ed935fbe1a9
Nico Glas <info@nicoglas.com>
2024-12-01 23:09:55 +0100

    [FAILED: Compilation error] day1: first run
    Might need to put error messages here for the next one

--- Day1.fsx
+++ Day1.fsx
@@ -1,8 +1,12 @@
-// #load "Data.fsx"
+#load "Data.fsx"
+#load "Utils.fsx"
 #r "nuget:Expecto"
 open Expecto
 open System
+open Data
+open Utils
 
+let mutable print = printResult 1 1
 let testList1 = [3;4;2;1;3;3]
 let testList2 = [4;3;5;3;9;3]
 
@@ -10,7 +14,11 @@ let s1 = List.sort testList1
 let s2 = List.sort testList2
 let absDiff (a:int,b:int) = Math.Abs (a-b)
 
-let s = List.zip s1 s2 |> List.map (absDiff)
+let solution (l,r) = List.zip l r |> List.map (absDiff)
+
+let s = solution (s1,s2)
 
 let expectedList = [2;1;0;1;2;5]
 Expect.equal s expectedList "Zip error somewhere"
+
+InputData.day1 () |> solution |> print
