62e632f9a665a3e95438f772630eccb77437c413
Nico Glas <info@nicoglas.com>
2024-12-01 23:44:16 +0100

    added scratch file
    using this to actually test my data import code.
    It currently returns the lists in reverse order,
    maybe that messes up the pairing in some way?

---
+++ scratch.fsx
@@ -0,0 +1,20 @@
+open System
+
+let private split (c: char) (input: string) =
+    input.Split(c, StringSplitOptions.RemoveEmptyEntries) |> List.ofArray
+
+let rec fn left right =
+    function
+    | [] -> left, right
+    | x :: xs ->
+        let (l, r) =
+            let s = x |> split ','
+            int s[0], int s[1]
+
+        let left' = l :: left
+        let right' = r :: right
+        fn left' right' xs
+
+let testinput = [ "3,4"; "4,3"; "2,5"; "1,3"; "3,9"; "3,3" ]
+
+fn [] [] testinput |> printfn "%A"
