9ee0ecdd16f24cb094d2937ce14599caa67e7b5a
Nico Glas <info@nicoglas.com>
2024-12-02 21:06:46 +0100

    [TEST FAILED] Nothing is safe anymore
    Everything is coming back `false`. probably means that
    my predicates are to strict in a way

---
+++ Day2.fsx
@@ -0,0 +1,45 @@
+#load "Data.fsx"
+#load "Utils.fsx"
+#r "nuget:Expecto"
+open Expecto
+open System
+open Data
+open Utils
+
+let data =
+    [ [ 7; 6; 4; 2; 1 ]
+      [ 1; 2; 7; 8; 9 ]
+      [ 9; 7; 6; 2; 1 ]
+      [ 1; 3; 2; 4; 5 ]
+      [ 8; 6; 4; 4; 1 ]
+      [ 1; 3; 6; 7; 9 ] ]
+
+let check report = 
+  let getOp = function
+  | [] -> failwith "damn"
+  | _::[] -> failwith "damn"
+  | x::y::_ -> match sign (x-y) with
+                | -1 -> (>)
+                | _ -> (<)
+
+
+  let rec fn op acc = function
+    | [] -> acc
+    | _::[] -> acc
+    | x::y::xs ->
+      let p = 
+        let inrange x = x >= 1 && x <= 3
+        x-y |> abs |> inrange
+      let p2 = op x y
+      let acc' = acc && p && p2
+      fn op acc' xs
+  let op = getOp report
+
+  fn op true report
+
+let actual = data |> List.map (check)
+let expected = [true;false;false;false;false;true]
+
+Expect.equal actual expected "'Cause your friends don't dance, and if you don't dance, then you ain't no friend of mine";
+
+
