46dc6e8e24878162357ff0a9b47a78bbecc25f28
Nico Glas <info@nicoglas.com>
2024-12-03 10:10:52 +0100

    [DAY 2 SOVLED]

--- Day3.fsx
+++ Day3.fsx
@@ -10,16 +10,25 @@ open System.Text.RegularExpressions
 
 let print (result: int) = printResult 2 1 result
 
-let data = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+let printPassThru s o =
+    printfn s o
+    o
 
-let ignoreExpression = Regex("don't\(\).*do\(\)")
+let data =
+    "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+do()mul(8,5)don't()(mul(11,8)undon()?mul(8,5))"
+
+let ignoreExpression = Regex("don't\(\).*?do\(\)")
+let secondPass input = Regex("don't\(\).*").Replace(input, "")
 let rex = Regex("mul\((\d*),(\d*)\)", RegexOptions.Compiled)
 
-let solution input = 
-  let mul input = 
-    [for x in rex.Matches(input) -> (int x.Groups[2].Value) * (int x.Groups[1].Value)]
-  ignoreExpression.Replace(input,"") |> mul |> List.sum
+let solution input =
+    let mul input =
+        [ for x in rex.Matches(input) -> (int x.Groups[2].Value) * (int x.Groups[1].Value) ]
 
-Expect.equal (solution data) 48
+    let replaced = ignoreExpression.Replace(input, "")
+
+    replaced |> secondPass |> mul |> List.sum
+
+Expect.equal (solution data) 48 "Maybe I should have used this stupid argument :facepalm:"
 
 InputData.day3 () |> solution |> print
