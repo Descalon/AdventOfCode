bca8cebd9f99f65ff58047a6b3f320f60721ed1a
Nico Glas <info@nicoglas.com>
2024-12-03 09:18:54 +0100

    [TEST PASSED] day 3 part 2 is going swimmingly

--- Day3.fsx
+++ Day3.fsx
@@ -10,12 +10,16 @@ open System.Text.RegularExpressions
 
 let print (result: int) = printResult 2 1 result
 
-let data = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+let data = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
+let ignoreExpression = Regex("don't\(\).*do\(\)")
 let rex = Regex("mul\((\d*),(\d*)\)", RegexOptions.Compiled)
 
-let solution input = [for x in rex.Matches(input) -> (int x.Groups[2].Value) * (int x.Groups[1].Value)] |> List.sum
+let solution input = 
+  let mul input = 
+    [for x in rex.Matches(input) -> (int x.Groups[2].Value) * (int x.Groups[1].Value)]
+  ignoreExpression.Replace(input,"") |> mul |> List.sum
 
-Expect.equal (solution data) 161
+Expect.equal (solution data) 48
 
-InputData.day3 () |> solution |> print
+// InputData.day3 () |> solution |> print
